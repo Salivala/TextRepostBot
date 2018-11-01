2888e4fad744306656beb0d6752be96bc54868e7
hsszyman <hsszyman@gmail.com>
2018-10-31 21:07:50 -0400

    No longer flags bots own posts as reposts. Removed DataSetManager. DataSet encapsulates all current file io and searching tasks,

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import praw
 from DataSet import DataSet
-from praw.models import Redditor
 from praw.models import Comment
 
 
@@ -8,18 +7,20 @@ reddit = praw.Reddit("bot1")
 subreddit = reddit.subreddit("toundrabotplayground")
 
 saved_data = DataSet("saved_info.txt")
-for submission in subreddit.hot(limit=500):
-    submission.comments.replace_more()
-    comment: Comment = None
-    for comment in submission.comments.list():
-        if saved_data.is_in("id", comment.id) is None:
-            print("new comment")
-            if saved_data.is_in("body", comment.body) is not None:
-                comment.reply("flagging for repost")
-                print("flagging " + comment.id + " for repost")
-                saved_data.add({"id": comment.id, "body": comment.body, "flagged": "t"})
+while True:
+    for submission in subreddit.hot(limit=500):
+        submission.comments.replace_more()
+        comment: Comment = None
+        for comment in submission.comments.list():
+            author_name = comment.author.name
+            if saved_data.is_in("id", comment.id) is None:
+                print("new comment")
+                if saved_data.is_in("body", comment.body) is not None and author_name != 'RepostDeputy':
+                    comment.reply("flagging for repost")
+                    print("flagging " + comment.id + " for repost")
+                    saved_data.add({"id": comment.id, "body": comment.body, "flagged": "t"})
+                else:
+                    saved_data.add({"id": comment.id, "body": comment.body, "flagged": "f"})
             else:
-                saved_data.add({"id": comment.id, "body": comment.body, "flagged": "f"})
-        else:
-            print("it's already there!")
+                print("it's already there!")
 
